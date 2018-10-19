5caffae747cef40d5d28c8d57f49fa6f3a4918ef
DineshkumarN1987 <dineshkumar.n@ikomettech.com>
2018-10-19 12:52:16 +0530

    category detail page committed

--- src/ManasquanBank.Web/scripts/main.js
+++ src/ManasquanBank.Web/scripts/main.js
@@ -746,6 +746,12 @@ if ($('#personalLogin, #businessLogin, #mpersonalLogin, #mbusinessLogin').length
 }
 
 
+function onSubmit(token) {
+    document.getElementById('contactForm').submit();
+}
+function onSubmitNews(token) {
+    document.getElementById('joinemailForm').submit();
+}
 
 if ($('.joinEmail').length > 0) {
     $(document).ready(function () {
@@ -781,6 +787,9 @@ if ($('.joinEmail').length > 0) {
                         $(this).parent().removeClass("successForm");
                     }
                 });
+            },
+            submitHandler: function (form) {
+                grecaptcha.execute();
             },
             errorPlacement: function (error, element) { }
         });
@@ -856,9 +865,9 @@ $(document).ready(function () {
 $(document).ready(function () {
     $(".timehrsSection").clone().appendTo($(".cloneTimeHrs"));
 
-    /*if ($('.mapSection').length > 0) {
+    if ($('.mapSection').length > 0) {
         var map;
-        google.maps.event.addDomListener(window, "load", function() {
+        google.maps.event.addDomListener(window, "load", function () {
             var map = new google.maps.Map(document.getElementById("map_div"), {
                 center: new google.maps.LatLng(40.137508, -74.089629),
                 zoom: 11,
@@ -871,46 +880,46 @@ $(document).ready(function () {
             /*
              * marker creater function (acts as a closure for html parameter)
              */
-    /*function createMarker(options, html) {
-        var marker = new google.maps.Marker(options);
-        if (html) {
-            google.maps.event.addListener(marker, "click", function() {
-                infoWindow.setContent(html);
-                infoWindow.open(options.map, this);
+            function createMarker(options, html) {
+                var marker = new google.maps.Marker(options);
+                if (html) {
+                    google.maps.event.addListener(marker, "click", function () {
+                        infoWindow.setContent(html);
+                        infoWindow.open(options.map, this);
+                    });
+                }
+                return marker;
+            }
+            /*add markers to map*/
+            var marker0 = createMarker({
+                position: new google.maps.LatLng(40.137508, -74.089629),
+                map: map,
             });
-        }
-        return marker;
-    }*/
-    /*add markers to map*/
-    /*var marker0 = createMarker({
-        position: new google.maps.LatLng(40.137508, -74.089629),
-        map: map,
-    });
 
-    var marker1 = createMarker({
-        position: new google.maps.LatLng(40.070730, -74.046798),
-        map: map,
-    });
+            var marker1 = createMarker({
+                position: new google.maps.LatLng(40.070730, -74.046798),
+                map: map,
+            });
 
-    var marker2 = createMarker({
-        position: new google.maps.LatLng(40.069431, -74.128023),
-        map: map,
-    });
-    var marker3 = createMarker({
-        position: new google.maps.LatLng(40.201300, -74.255699),
-        map: map,
-    });
-    var marker4 = createMarker({
-        position: new google.maps.LatLng(40.120684, -74.144013),
-        map: map,
-    });
-});*/
-    /*}*/
+            var marker2 = createMarker({
+                position: new google.maps.LatLng(40.069431, -74.128023),
+                map: map,
+            });
+            var marker3 = createMarker({
+                position: new google.maps.LatLng(40.201300, -74.255699),
+                map: map,
+            });
+            var marker4 = createMarker({
+                position: new google.maps.LatLng(40.120684, -74.144013),
+                map: map,
+            });
+        });
+    }
 });
 $(document).ready(function () {
-    /*if ($('.mapLocation').length > 0) {
+    if ($('.mapLocation').length > 0) {
         var map;
-        google.maps.event.addDomListener(window, "load", function() {
+        google.maps.event.addDomListener(window, "load", function () {
             var map = new google.maps.Map(document.getElementById("map_div2"), {
                 center: new google.maps.LatLng(40.137508, -74.089629),
                 zoom: 11,
@@ -923,39 +932,39 @@ $(document).ready(function () {
             /*
              * marker creater function (acts as a closure for html parameter)
              */
-    /*function createMarker(options, html) {
-        var marker = new google.maps.Marker(options);
-        if (html) {
-            google.maps.event.addListener(marker, "click", function() {
-                infoWindow.setContent(html);
-                infoWindow.open(options.map, this);
+            function createMarker(options, html) {
+                var marker = new google.maps.Marker(options);
+                if (html) {
+                    google.maps.event.addListener(marker, "click", function () {
+                        infoWindow.setContent(html);
+                        infoWindow.open(options.map, this);
+                    });
+                }
+                return marker;
+            }
+            /*add markers to map*/
+            var marker0 = createMarker({
+                position: new google.maps.LatLng(40.137508, -74.089629),
+                map: map,
             });
-        }
-        return marker;
-    }*/
-    /*add markers to map*/
-    /*var marker0 = createMarker({
-        position: new google.maps.LatLng(40.137508, -74.089629),
-        map: map,
-    });
 
-    var marker1 = createMarker({
-        position: new google.maps.LatLng(40.070730, -74.046798),
-        map: map,
-    });
+            var marker1 = createMarker({
+                position: new google.maps.LatLng(40.070730, -74.046798),
+                map: map,
+            });
 
-    var marker2 = createMarker({
-        position: new google.maps.LatLng(40.069431, -74.128023),
-        map: map,
-    });
-    var marker3 = createMarker({
-        position: new google.maps.LatLng(40.201300, -74.255699),
-        map: map,
-    });
-    var marker4 = createMarker({
-        position: new google.maps.LatLng(40.120684, -74.144013),
-        map: map,
-    });
-});*/
-    /*}*/
+            var marker2 = createMarker({
+                position: new google.maps.LatLng(40.069431, -74.128023),
+                map: map,
+            });
+            var marker3 = createMarker({
+                position: new google.maps.LatLng(40.201300, -74.255699),
+                map: map,
+            });
+            var marker4 = createMarker({
+                position: new google.maps.LatLng(40.120684, -74.144013),
+                map: map,
+            });
+        });
+    }
 });
