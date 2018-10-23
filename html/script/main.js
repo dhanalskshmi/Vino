53d909391b8c9c2daf022adad91a6c3d2c1779e8
ArunBharathi <arun@ikometsolutions.com>
2018-10-23 21:04:27 +0530

    global form json added

--- html/script/main.js
+++ html/script/main.js
@@ -1033,13 +1033,24 @@ function GetReCaptchaID(containerID) {
 function onSubmit(token) {
   document.getElementById('contactForm').submit();
 }
-if ($('#contactForm').length > 0) {
+$( window ).on("load", function() {
+ $('.selectBox').each(function(e) {
+    var name = $(this);
+    $(this).parent().find('label').attr('for', name.attr('name'));
+});
+});
+
+if ($('.contactForm').length > 0) {
     $(document).ready(function () {
+     
         jQuery.validator.addMethod("EmailVal", function (e, t) {
             var o = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return this.optional(t) || o.test(e)
         }, "Please enter a valid email address.")
-        var validator = $('#contactForm').validate({
+
+        jQuery.validator.addMethod("notEqual", function (value, element, param) { return this.optional(element) || value != $(param).val(); }, "This has to be different...");
+
+        var validator = $('.contactForm').validate({
             rules: {
                 cntTelePhone: {
                     minlength: 13
@@ -1049,38 +1060,53 @@ if ($('#contactForm').length > 0) {
                 },
             },
             ignore: [],
-            highlight: function (element, errorClass) {
-                var selector = "#" + element.id;
-                $(selector).addClass(errorClass);
+            
+            errorPlacement: function (error, element) {
+            return true;
+        },
+        highlight: function (element, errorClass) {
+            var selector = "#" + element.id;
+            $(selector).addClass(errorClass);
 
-                $(selector).parent().find("span.vd").removeClass('f-important f-success').addClass('f-error');
-                $(selector).parent().removeClass("successForm");
-                $(selector).parent().addClass("errorForm");
-            },
-            unhighlight: function (element, errorClass) {
+            $(selector).parent().find("span.vd").removeClass('f-important f-success').addClass('f-error');
+        },
+        unhighlight: function (element, errorClass) {
 
-                var selector = "#" + element.id;
-                $(selector).removeClass(errorClass);
-                $(selector).parent().removeClass("errorForm");
-                $(selector).parent().find("span.vd").removeClass('f-important f-error').addClass('f-success');
-                $(selector).parent().addClass("successForm");
-                $('input[type="text"]').each(function () {
-                    if ($(this).val() == "") {
-                        $(this).parent().removeClass("successForm");
-                    }
-                });
-            },
+            var selector = "#" + element.id;
+            $(selector).removeClass(errorClass);
+            $(selector).parent().find("span.vd").removeClass('f-important f-error').addClass('f-success');
+        },
             submitHandler: function (form) {
                 var reCaptchaID = GetReCaptchaID("capt_contact");
                 grecaptcha.reset(reCaptchaID);
                 grecaptcha.execute(reCaptchaID);
             },
-            errorPlacement: function (error, element) { }
+            // errorPlacement: function (error, element) { 
+            //     return true;
+            // }
         });
         jQuery("#cntTelePhone").length > 0 && document.getElementById("cntTelePhone").addEventListener("input", function (e) {
             var t = e.target.value.replace(/\D/g, "").match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
             e.target.value = t[2] ? "(" + t[1] + ") " + t[2] + (t[3] ? "-" + t[3] : "") : t[1]
         })
+          $('.contactForm input').keydown(function(event) {
+        var keycode = (event.keyCode ? event.keyCode : (event.which ? event.which : event.charCode));
+        if  ( keycode  == 13) {   // key code for enter key
+
+        return false;
+        } else  {
+
+        validator.checkForm();
+        }
+    });
+
+    $('.contactForm .selectBox').not("select.multiple,#state,#country").selectmenu({
+        style: 'dropdown',
+        transferClasses: true,
+        change: function() {
+          $('.contactForm').validate().element(this);
+       }
+      });
     });
 }
 $(document).ready(function () {
@@ -1094,13 +1120,8 @@ $(document).ready(function () {
         });
     }
 
-    $('.contactForm .selectBox').not("select.multiple").selectmenu({
-        style: 'dropdown',
-        transferClasses: true,
-        change: function() {
-          $('#form_module').validate().element(this);
-       }
-      });
+   
+
 });
 
 if ($('.timelineWrappers').length > 0) {
@@ -1801,10 +1822,3 @@ if ($(window).width() <= 1023) {
 }
 
 });
-  $(document).ready(function(){
-  $(".selectBox").selectmenu();
-
- 
-
-  });
-   
